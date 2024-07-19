d54e56debd47a29d9866c3a0a7314b80baf4299d
zafri23 <zafrianuar23@gmail.com>
2024-07-19 16:27:22 +0800

    style

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -6,7 +6,7 @@ class LoadingBar{
 		this.domElement.style.left = '0';
 		this.domElement.style.width = '100%';
 		this.domElement.style.height = '100%';
-		this.domElement.style.background = '#000';
+		this.domElement.style.background = '#000
 		this.domElement.style.opacity = '0.7';
 		this.domElement.style.display = 'flex';
 		this.domElement.style.alignItems = 'center';
